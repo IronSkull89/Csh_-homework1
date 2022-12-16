ea4b8bf85cf0376d402d53b202e473935f4639a8
Iron Skull <ironskull89@mail.ru>
2022-12-16 21:10:50 +0300

    added solution of four home tasks

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine());
+
+// int max = a;
+
+// if (max < b) max = b;
+
+// Console.WriteLine("Максимальное число: " + max);
+
+//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int c = int.Parse(Console.ReadLine());
+
+// int max = a;
+
+// if (max < b) max = b;
+// if (max < c) max = c;
+
+// Console.WriteLine("Максимальное число: " + max);
+
+//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.Write("Введите число: ");
+// int a = int.Parse(Console.ReadLine());
+// if (a % 2 == 0) Console.WriteLine($"Число {a} является чётным.");
+// else Console.WriteLine($"Число {a} является нечётным.");
+
+//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Console.Write("Введите верхнюю границу диапазона: ");
+// int N = int.Parse(Console.ReadLine());
+// int a = 2;
+
+// Console.Write($"Чётные числа от 1 до {N}:");
+// while (a <= N)
+// {
+//   Console.Write(" " + a);  
+//   a += 2;
+// };
